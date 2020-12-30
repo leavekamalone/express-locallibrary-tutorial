ae817d690be5c596f675a6da40bb5f52df35722d
Kamalpreet Bhatti <kamalpreet.bhatti@gmail.com>
2020-12-29 21:39:49 -0500

    Added files and changes required for deployment to heroku

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -185,7 +185,9 @@ body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).is
                 first_name: req.body.first_name,
                 family_name: req.body.family_name,
                 date_of_birth: req.body.date_of_birth,
-                date_of_death: req.body.date_of_death
+                date_of_death: req.body.date_of_death,
+                _id:req.params.id 
+                
             });
 
         if (!errors.isEmpty()) {
